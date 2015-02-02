d8d7480067523075aeba1eb8e13080f6103f6553
Dotun Olusoga <adedotun.olusoga@gmail.com>
2015-02-02 11:39:33 -0600

    use IIFE

--- main.js
+++ main.js
@@ -1,31 +1,58 @@
-$.get('https://yspuku7qvh9u4cr3.firebaseio.com/.json', function(res){
-  var data = res['c8-students'];
-  var $ul = $('ul');
+(function() {
+  'use strict';
 
-  console.log(_.VERSION);
 
-  var chunkedStudents = _(data).map(function(value){
-    return value.firstName + ' ' + value.lastName[0] + '.';
-  })
-  .shuffle()
-  .chunk(4)
-  .value();
-  var $ul = $('ul');
-  $ul.append(createList(chunkedStudents));
-});
+  var url  = 'https://yspuku7qvh9u4cr3.firebaseio.com/.json';
+  var $ul;
+  var $input;
 
-function createList(array) {
-  var groupList = [];
+  $(document).ready(init);
 
-  _.forEach(array, function(team){
-    var $ol = $('<ol></ol>');
 
-    _.forEach(team, function(teamMember){
-      var $li = $('<li>' + teamMember + '</li>');
-      $ol.append($li);
-    })
-    groupList.push($ol);
-  })
-  return groupList;
-}
+  function init() {
+    $input = $('input');
+    $ul    = $('ul');
+
+    $input.change(getUpdateAndSplit);
+    getUpdateAndSplit();
+
+  }
+
+  function getUpdateAndSplit() {
+    var count = $input.val();
+
+    $ul.empty();
+    $.get(url, function(res) {
+      var chunkedStudents = chunkData(res['c8-students'], count);
+      $ul.append(createList(chunkedStudents));
+    });
+  };
+
+  function chunkData(data, count) {
+    return _(data)
+      .map(function (value) {
+        return value.firstName + ' ' + value.lastName[0] + '.';
+      })
+    .shuffle()
+    .chunk(count)
+    .value();
+  }
+
+  function createList(array) {
+    var groupList = [];
+
+    _.forEach(array, function(team){
+      var $ol = $('<ol></ol>');
+
+      _.forEach(team, function(teamMember){
+        var $li = $('<li>' + teamMember + '</li>');
+        $ol.append($li);
+      });
+
+      groupList.push($ol);
+    });
+
+    return groupList;
+  }
+}());
 
